fd0796a16e4d05437d5f940da48ded69f87bb255
ktl014 <ktl014@ucsd.edu>
2018-06-26 16:43:57 -0700

    Update to NN scripts to avoid errors

--- pose/poseprediction_torch/class_train.py
+++ pose/poseprediction_torch/class_train.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 import torch.nn as nn
 from torch.nn import init
